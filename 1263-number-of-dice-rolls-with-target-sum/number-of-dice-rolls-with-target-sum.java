8f3a7ad24d62082e8d12fe03fa7ff0e6ac83a0c0
Ut5472 <140581917+Ut5472@users.noreply.github.com>
2026-07-10 01:41:39 +0530

    Time: 32 ms (5.83%) | Memory: 45.9 MB (8.38%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -1,7 +1,7 @@
 class Solution {
     private int diceRoller(int n,int k,int target,int[][] dp){
         int MOD = 1000000007;
-        if(target<0 ){
+        if(target<0){
             return 0;
         }
         if(n==0 && target==0){
@@ -23,11 +23,34 @@ class Solution {
         dp[n][target] = (int)ans;
         return (int)ans;
     }
+    private int tabulation(int n,int k,int target){
+        int MOD = 1000000007;
+        int[][] dp = new int[n+1][target+1];
+        for(int[] row:dp){
+            Arrays.fill(row,0);
+        }
+        dp[0][0] = 1;
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                ans = 0;
+                for(int K=1;K<=k;K++){
+                    if(j-K>=0){
+                        ans = (ans%MOD + dp[i-1][j-K]%MOD)%MOD; 
+                    }
+                }
+                dp[i][j] = (int)ans;
+            }
+        }
+        return dp[n][target];
+
+    }
     public int numRollsToTarget(int n, int k, int target) {
         int[][] dp = new int[n+1][target+1];
         for(int[] row:dp){
             Arrays.fill(row,-1);
         }
-        return diceRoller(n,k,target,dp);
+       // return diceRoller(n,k,target,dp);
+       return tabulation(n,k,target);
     }
 }
