c64beff5ad100374849bfe8bc634fb020e064f78
Ut5472 <140581917+Ut5472@users.noreply.github.com>
2026-07-11 15:35:34 +0530

    Time: 31 ms (11.33%) | Memory: 53.5 MB (89.17%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -17,8 +17,9 @@ class Solution {
             ans1 = Math.max(ans1,ans2);
         }
         dp[i][j] = Math.max(ans1,ans);
-        return Math.max(ans1,ans);
+        return  dp[i][j];
     }
+   
     public int longestCommonSubsequence(String text1, String text2) {
         int[][] dp = new int[text1.length()+1][text2.length()+1];
         for(int[] row:dp){
