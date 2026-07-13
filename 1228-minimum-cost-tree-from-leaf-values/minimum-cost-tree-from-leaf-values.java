9fda8cb8c0453b98b40e57f6802baa40ea7ae0a1
Ut5472 <140581917+Ut5472@users.noreply.github.com>
2026-07-13 23:43:29 +0530

    Time: 8 ms (6.18%) | Memory: 43.5 MB (10.62%) - LeetSync

--- 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.java
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.java
@@ -21,15 +21,33 @@ class Solution {
         }
         dp[i][j] = min;
         return min;
+    }
+    private int tabulation(int[] arr,int[][] matrix){
+        int n = arr.length;
+        int[][] dp = new int[n+1][n+1];
+        //int min = Integer.MAX_VALUE;
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                int min = Integer.MAX_VALUE;
+                for(int part = i;part<j;part++){
+                    int ans = dp[i][part];
+                    int ans2 = dp[part+1][j];
+                    int temp = matrix[i][part]*matrix[part+1][j] + ans + ans2;
+                    min = Math.min(temp,min);
+                }
+                dp[i][j] = min;
+            }
+        }
+        return dp[0][n-1];
 
     }
+
     public int mctFromLeafValues(int[] arr) {
         int n = arr.length;
         int[][] dp = new int[n+1][n+1];
         for(int[] row:dp){
             Arrays.fill(row,-1);
         }
-        
         int[][] matrix = new int[n][n];
         for(int i=0;i<n;i++){
             matrix[i][i] = arr[i];
@@ -37,6 +55,7 @@ class Solution {
                 matrix[i][j] = Math.max(matrix[i][j-1],arr[j]); 
             }
         }
-        return solver(arr,matrix,0,n-1,dp);
+        return tabulation(arr,matrix);
+        //return solver(arr,matrix,0,n-1,dp);
     }
 }
