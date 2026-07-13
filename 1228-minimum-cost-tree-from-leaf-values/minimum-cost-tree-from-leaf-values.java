9136e4ec4f3d2c7fb3358b1e36e9fe8e61518159
Ut5472 <140581917+Ut5472@users.noreply.github.com>
2026-07-13 23:11:41 +0530

    Time: 1384 ms (5.21%) | Memory: 46.9 MB (5.21%) - LeetSync

---
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.java
@@ -0,0 +1,42 @@
+class Solution {
+    private int solver(int[] arr,int[][] matrix,int i,int j,int[][] dp){
+        //base case
+        if(i>=j){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        //recursion
+        int min = Integer.MAX_VALUE;
+        for(int part=i;part<j;part++){
+            int ans =  solver(arr,matrix,i,part,dp);
+            int ans2 = solver(arr,matrix,part+1,j,dp);
+            int temp = matrix[i][part] * matrix[part+1][j] + ans + ans2;
+            min = Math.min(temp,min);      
+            System.out.println(temp);
+            System.out.println(ans);
+            System.out.println(ans2);
+            System.out.println("\n");      
+        }
+        dp[i][j] = min;
+        return min;
+
+    }
+    public int mctFromLeafValues(int[] arr) {
+        int n = arr.length;
+        int[][] dp = new int[n+1][n+1];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
+        
+        int[][] matrix = new int[n][n];
+        for(int i=0;i<n;i++){
+            matrix[i][i] = arr[i];
+            for(int j=i+1;j<n;j++){
+                matrix[i][j] = Math.max(matrix[i][j-1],arr[j]); 
+            }
+        }
+        return solver(arr,matrix,0,n-1,dp);
+    }
+}
