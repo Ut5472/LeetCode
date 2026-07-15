cdb207465b69ed4922d69e7eeee42d4bd19d3b89
Ut5472 <140581917+Ut5472@users.noreply.github.com>
2026-07-15 13:01:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (57.16%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,19 +1,7 @@
 class Solution {
-    public int gcdOfOddEvenSums(int N) {
-        if(N==1){
-            return 1;
-        }
-        double n = (double)(N);
-        double sumOdd = n/2*(2+(n-1)*2);
-        double sumEven = n/2*(4+(n-1)*2);
-        double min = Math.min(sumEven,sumOdd);
-        int ans = 0;
-        for(int i=2;i<=min;i++){
-            if(sumOdd%i == 0 && sumEven%i==0){
-                ans = i;
-            }
-        }if(ans==0){
-         ans = (int)sumOdd*(int)sumEven;}
-        return ans;
+    public int gcdOfOddEvenSums(int n) {
+        int odd = n*n;
+        int even = n*(n+1);
+        return n;
     }
 }
