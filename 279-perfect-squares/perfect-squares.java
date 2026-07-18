94d02000ce53f1c3788749bb99b4a627edfff30d
Ut5472 <140581917+Ut5472@users.noreply.github.com>
2026-07-18 15:09:21 +0530

    Time: 27 ms (78.36%) | Memory: 46.1 MB (17.48%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -19,9 +19,30 @@ class Solution {
         }
         return dp[n] = ans;
     }
+    private int tabulation(int n){
+        int[] dp = new int[n+1];
+        dp[n] = 0;
+        for(int k=1;k<=n;k++){
+            int ans = Integer.MAX_VALUE;
+            int i = 1;
+            int end = (int)Math.sqrt(k);
+            while(i<=end){
+                int sq = i*i;
+                int reans = 1 + dp[k-sq];
+                i++;
+                if(reans<ans){
+                    ans = reans;
+                }
+            }
+            dp[k] = ans;
+        }
+        return dp[n];
+        
+    }
     public int numSquares(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        return Squares(n,dp);
+        //return Squares(n,dp);
+        return tabulation(n);
     }
 }
