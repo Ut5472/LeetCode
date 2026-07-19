563779409392cfdeab4963dba23f3d52faa379ac
Ut5472 <140581917+Ut5472@users.noreply.github.com>
2026-07-19 15:52:26 +0530

    Time: 2 ms (86.89%) | Memory: 43.8 MB (45.69%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] freq = new int[26];
+        boolean[] visited = new boolean[26];
+
+        // Count frequency
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']--;
+
+            // Already in answer
+            if (visited[ch - 'a']) {
+                continue;
+            }
+
+            // Remove larger characters if they appear later
+            while (!stack.isEmpty()
+                    && stack.peek() > ch
+                    && freq[stack.peek() - 'a'] > 0) {
+
+                visited[stack.pop() - 'a'] = false;
+            }
+
+            stack.push(ch);
+            visited[ch - 'a'] = true;
+        }
+
+        // Build answer
+        StringBuilder ans = new StringBuilder();
+        for (char ch : stack) {
+            ans.append(ch);
+        }
+
+        return ans.toString();
+    }
+}
