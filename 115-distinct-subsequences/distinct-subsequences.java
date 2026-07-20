471a46ae0fe9771eddbb333bd1775abfa5d56eaa
Ut5472 <140581917+Ut5472@users.noreply.github.com>
2026-07-20 20:44:23 +0530

    Time: 21 ms (35.33%) | Memory: 62.9 MB (12.41%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -23,12 +23,29 @@ class Solution {
         }
         return dp[i][j] = ans;
     }
+    private int tabulation(String s,String t){
+        int[][] dp = new int[s.length()+1][t.length()+1];
+        for(int i=0;i<=s.length();i++){
+            dp[i][t.length()] = 1;
+        }
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = t.length() - 1; j >= 0; j--) {
+                if (s.charAt(i) == t.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                } else {
+                    dp[i][j] = dp[i + 1][j];
+                }
+            }
+}
+        return dp[0][0];
+    }
     public int numDistinct(String s, String t) {
         //String build = new String();
         int[][] dp = new int[s.length()+1][t.length()+1];
         for(int[] row:dp){
             Arrays.fill(row,-1);
         }
-        return distinct(s,t,0,0,dp);
+        //return distinct(s,t,0,0,dp);
+        return tabulation(s,t);
     }
 }
