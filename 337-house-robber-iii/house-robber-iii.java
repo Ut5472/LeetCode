56d804b4ba1cdbabdde662e0aa9517929dc7711d
Ut5472 <140581917+Ut5472@users.noreply.github.com>
2026-07-22 02:28:42 +0530

    Time: 2 ms (28.10%) | Memory: 46.9 MB (18.77%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.java
+++ 337-house-robber-iii/house-robber-iii.java
@@ -23,36 +23,17 @@ class Solution {
             return dp.get(root);
         }
         //recursion
-        TreeNode left_child = null ;
-        TreeNode right_child = null ;
-        TreeNode left_left_child = null;
-        TreeNode left_right_child = null;
-        TreeNode right_left_child = null;
-        TreeNode right_right_child = null;
-        if(root.left!=null){
-            left_child = root.left;
-            //taking two child
-            if(left_child.left!=null){
-                left_left_child = left_child.left;
-            }
-            if(left_child.right!=null){
-                left_right_child = left_child.right;
-            }
+        int take = root.val;
+        if (root.left != null) {
+            take += lootCalculater(root.left.left, dp);
+            take += lootCalculater(root.left.right, dp);
         }
-        if(root.right!=null){
-            right_child = root.right;
-            if(right_child.left!=null){
-                right_left_child = right_child.left;
-            }
-            if(right_child.right!=null){
-                right_right_child = right_child.right;
-            }
-
+        if (root.right != null) {
+            take += lootCalculater(root.right.left, dp);
+            take += lootCalculater(root.right.right, dp);
         }
-        ////taking the current node to the answer
-        int take = root.val + lootCalculater(left_left_child,dp) + lootCalculater(left_right_child,dp) + lootCalculater(right_left_child,dp) + lootCalculater(right_right_child,dp);
         //not taking the current
-        int leave = lootCalculater(left_child,dp) + lootCalculater(right_child,dp);
+        int leave = lootCalculater(root.left,dp) + lootCalculater(root.right,dp);
         dp.put(root,Math.max(take,leave));
         return Math.max(take,leave);
         
