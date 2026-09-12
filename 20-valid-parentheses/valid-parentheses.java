e30e28053e1299d50c5e194b16ea1d47db566777
Ut5472 <140581917+Ut5472@users.noreply.github.com>
2026-09-13 03:11:34 +0530

    Time: 3 ms (85.90%) | Memory: 43.5 MB (15.11%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk=new Stack();
+        for(char c: s.toCharArray())
+        {
+            if(c=='(' || c=='{' || c=='[')
+            {
+                stk.push(c);
+            }
+            else
+            {
+                if(stk.isEmpty())
+                    return false;
+                char top=stk.pop();
+                if((top!='{' && c=='}') || (top!='(' && c==')') || (top!='[' && c==']'))
+                {
+                    return false;
+                } 
+            }
+        }  
+        return stk.isEmpty();
+    }
+}
